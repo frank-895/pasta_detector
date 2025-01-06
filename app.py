dc5223f0607d8cbb32b55f63fd2b1634e65d6309
Frank <frank.snelling03@gmail.com>
2025-01-06 17:11:52 +0000

    added examples for web app

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ def predict(img):
     _, _, probs = learn.predict(img)
     return {labels[i]: float(probs[i]) for i in range(len(labels))}
 
+# to generate examples
+pastas = ['spaghetti', 'penne', 'fusilli', 'farfalle', 'fettuccine', 'macaroni', 'orecchiette', 'gnocchi']
+
 gr.Interface(
     fn=predict,
     inputs=gr.Image(height=512, width=512),
     outputs=gr.Label(num_top_classes=3),
     title="Pasta Detector",
-    description="A pasta classifier trained on duckduckgo searches"
+    description="A pasta classifier trained on duckduckgo searches",
+    examples=[f"images/{pasta}" for pasta in pastas]
 ).launch(share=True)
