2c5ff34bf64b97ba661c2708481d70577e58ae0c
Frank <frank.snelling03@gmail.com>
2025-01-06 16:48:08 +0000

    added gradio app file and exported model

---
+++ app.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from fastai.vision.all import *
+
+learn = load_learner('export.pkl')
+
+labels = learn.dls.vocab
+def predict(img):
+    img = PILImage.create(img)
+    _, _, probs = learn.predict(img)
+    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+
+gr.Interface(
+    fn=predict,
+    inputs=gr.Image(height=512, width=512),
+    outputs=gr.Label(num_top_classes=3),
+    title="Pasta Detector",
+    description="A pasta classifier trained on duckduckgo searches"
+).launch(share=True)
